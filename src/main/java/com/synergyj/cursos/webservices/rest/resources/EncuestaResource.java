229561943818bc6f077d7e621c63e6b91e9be60b
edgar bernal remoto <tecualoyaster@gmail.com>
2014-09-06 13:34:21 -0500

    lab4 anotaciones jersey
    se usan anotación FormParam para acceder a los controle de la forma

--- src/main/java/com/synergyj/cursos/webservices/rest/resources/EncuestaResource.java
+++ src/main/java/com/synergyj/cursos/webservices/rest/resources/EncuestaResource.java
@@ -32,8 +32,14 @@ public class EncuestaResource {
 	private static final Logger logger = LoggerFactory.getLogger(EncuestaResource.class);
 
 	@POST
+	/*
+	 * para decir que si viene de una forma
+	 */
 	@Consumes("application/x-www-form-urlencoded")
 	@Produces("text/html")
+	/*
+	 * se usar @FormParam para accesar los controles de la forma, el atributo para identificar es el name
+	 */
 	public String guardaEncuesta(@FormParam("nombre") String nombre,
 			@FormParam("cursoPreferido") String cursoPreferido, @FormParam("edad") int edad) {
 
